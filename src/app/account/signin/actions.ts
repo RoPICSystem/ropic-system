7edb3e38ab9e3fa15389f689a4a5a2783d7b0ca9
RenzVillegas24 <renzjustinevillegas@gmail.com>
2025-05-23 17:55:02 +0800

    feat: add 'Warehouses' navigation item for admin users in sidebar and breadcrumbs

--- src/app/account/signin/actions.ts
+++ src/app/account/signin/actions.ts
@@ -10,7 +10,6 @@ declare global {
     userData?: any;
   }
 }
-
 export async function signin(formData: FormData):
   Promise<{ error?: string, success?: boolean }> {
   const supabase = await createClient()
@@ -30,7 +29,6 @@ export async function signin(formData: FormData):
     return { error: error.message }
   }
 
-
   const { data, error: userError } = await getUserProfile();
 
   if (userError) {
@@ -38,15 +36,28 @@ export async function signin(formData: FormData):
     return { error: `${userError}`}
   }
 
+  // wait fot the window to be defined
   if (typeof window !== "undefined") {
     window.userData = data;
   } else {
     return { error: "Window is not defined" }
   }
 
+  // Add the userdata in cookies
+  const { cookies } = await import('next/headers');
+  const essentialUserData = {
+    id: data?.id,
+    email: data?.email,
+    name: data?.full_name || data?.email
+  };
+  (await cookies()).set('userData', JSON.stringify(essentialUserData), {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 60 * 60 * 24 * 7, // 1 week
+    path: '/',
+  });
 
   console.log("User data:", data);
-
 
   // Revalidate cache to reflect the new authentication state
   revalidatePath('/', 'layout')
--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -42,11 +42,11 @@ export default function NavigationBread() {
         if (data?.is_admin) {
           setNavigation([
             { name: 'Dashboard', href: '/home/dashboard', icon: "heroicons:home-solid" },
+            { name: 'Warehouses', href: '/home/warehouses', icon: 'material-symbols:warehouse-rounded' },
             { name: 'Inventory', href: '/home/inventory', icon: "fluent:box-20-filled" },
             { name: 'Delivery', href: '/home/delivery', icon: "heroicons:truck-20-solid" },
             { name: 'Warehouse Items', href: '/home/warehouse-items', icon: "fluent:box-20-filled" },
             { name: 'Reorder Point', href: '/home/reorder-point', icon: "heroicons:chart-bar-20-solid" },
-            { name: 'Warehouses', href: '/home/warehouses', icon: 'material-symbols:warehouse-rounded' },
             { name: 'Notifications', href: '/home/notifications', icon: "heroicons:bell-alert-20-solid" },
             { name: 'Settings', href: '/home/settings', icon: "heroicons:cog-8-tooth-20-solid" },
             { name: 'Profile', href: '/home/profile', icon: "heroicons:user-20-solid" },
